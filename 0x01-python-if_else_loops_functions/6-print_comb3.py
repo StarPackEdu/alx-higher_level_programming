90fc786fd9910446b7813d37209f8a7cfa55a3e8
StarPackEdu <111011755+StarPackEdu@users.noreply.github.com>
2022-12-01 00:02:48 +0100

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,2 +1,7 @@
-th
-th
+#!/usr/bin/python3
+number = 0
+while number <= 89:
+    if number % 10 == 0:
+        number += 1 + number // 10
+    print("{:02d}".format(number), end='\n' if number == 89 else ", ")
+    number += 1
