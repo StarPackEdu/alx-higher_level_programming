e8fb81dab2218673b2e04ae40b965da41dadb42f
StarPackEdu <111011755+StarPackEdu@users.noreply.github.com>
2022-11-30 23:40:09 +0100

    Update 0-positive_or_negative.py

--- 0x01-python-if_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_loops_functions/0-positive_or_negative.py
@@ -1,2 +1,9 @@
-thr
-thr
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+if number > 0:
+    print("{} is positive".format(number))
+elif number == 0:
+    print("{} is zero".format(number))
+elif number < 0:
+    print("{} is negative".format(number))
