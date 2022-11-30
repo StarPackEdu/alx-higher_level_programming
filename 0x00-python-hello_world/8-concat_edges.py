87e572b7ab303ba053061af58476ae5944501b76
StarPackEdu <111011755+StarPackEdu@users.noreply.github.com>
2022-11-30 02:10:23 +0100

    Update 8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,2 +1,5 @@
-thr
-th
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+    language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[-22:-17] + str[:6]
+print(str)
