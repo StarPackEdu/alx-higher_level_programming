7ab0f2720d54b9485a52f4bc6e98b0c36015540d
StarPackEdu <111011755+StarPackEdu@users.noreply.github.com>
2022-12-04 18:18:10 +0100

    Update 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,2 +1,9 @@
-the
-th
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return ("None")
+    y = my_list[0]
+    for i in my_list:
+        if i > y:
+            y = i
+    return (y)
