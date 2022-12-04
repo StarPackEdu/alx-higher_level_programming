953976ad291085f4faa310888d395bfe22be64f1
StarPackEdu <111011755+StarPackEdu@users.noreply.github.com>
2022-12-04 18:15:11 +0100

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,2 +1,8 @@
-the
-th
+#!/usr/bin/python3
+
+
+def print_reversed_list_integer(my_list=[]):
+    if isinstance(my_list, list):
+        my_list.reverse()
+        for i in my_list:
+            print("{:d}".format(i))
