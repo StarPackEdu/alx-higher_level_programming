b80f064652d18652a59a33abca28c2f048aefe09
StarPackEdu <111011755+StarPackEdu@users.noreply.github.com>
2022-12-04 18:23:09 +0100

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
 
-def print_reversed_list_integer(my_list=[]):
-    if isinstance(my_list, list):
-        my_list.reverse()
-        for i in my_list:
-            print("{:d}".format(i))
+def new_in_list(my_list, idx, element):
+
+    if (idx < 0) or (idx > (len(my_list)-1)):
+        return my_list
+
+    copy = [x for x in my_list]
+    copy[idx] = element
+    return copy
