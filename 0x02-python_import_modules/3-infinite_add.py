b869cf889f347b21d3b3178c6c32bc458d080ff3
StarPackEdu <111011755+StarPackEdu@users.noreply.github.com>
2022-12-04 17:29:51 +0100

    Update 3-infinite_add.py

--- 0x02-python_import_modules/3-infinite_add.py
+++ 0x02-python_import_modules/3-infinite_add.py
@@ -1,2 +1,8 @@
-thr
-th
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    result = 0
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            result += int(arg)
+    print(result)
