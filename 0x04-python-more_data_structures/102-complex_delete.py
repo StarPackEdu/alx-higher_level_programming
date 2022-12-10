56d74c7b9e6de063a489272a1de63834882f02be
StarPackEdu <111011755+StarPackEdu@users.noreply.github.com>
2022-12-10 02:21:13 +0100

    Update 102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,1 +1,22 @@
-t11;rgb:0000/0000/0000he
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    """
+    Deletes keys with a specific value in a dictionary
+    ...
+
+    Parameters
+    ----------
+    a_dictionary : dictionary
+        the given dictionary to delete value from
+
+    Return:
+        the new dictionary
+    """
+
+    while value in a_dictionary.values():
+        for k, v in a_dictionary.items():
+            if v == value:
+                del a_dictionary[k]
+                break
+
+    return (a_dictionary)
