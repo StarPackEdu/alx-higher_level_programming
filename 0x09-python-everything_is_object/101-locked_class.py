3b97b836dbdb215d1624b72980a608724955a620
StarPackEdu <111011755+StarPackEdu@users.noreply.github.com>
2023-01-05 10:39:58 +0100

    Update 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,2 +1,14 @@
-t0;276;0che
-th
+#!/usr/bin/python3
+"""
+Module for a class that prevents dynamic attributes creation
+
+"""
+
+
+class LockedClass():
+    """Class to prevent dynamic attributes creation"""
+    __slots__ = ['first_name']
+
+    def __init__(self):
+        """Init method"""
+        pass
