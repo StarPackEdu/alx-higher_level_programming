637a1d778c482f8ffdf9980000d3f30d1dab5e35
StarPackEdu <111011755+StarPackEdu@users.noreply.github.com>
2023-01-13 10:02:31 +0100

    Update 4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,2 +1,9 @@
-the
-th
+#!/usr/bin/python3
+
+"""Defines a JSON-to-object function."""
+import json
+
+
+def from_json_string(my_str):
+    """Return the Python object representation of a JSON string."""
+    return json.loads(my_str)
