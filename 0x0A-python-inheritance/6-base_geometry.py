01fd32ef6ebe0006cfa0c9d82c339ee2528797c1
StarPackEdu <111011755+StarPackEdu@users.noreply.github.com>
2023-01-13 11:46:45 +0100

    Update 6-base_geometry.py

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,2 +1,11 @@
-the
-th
+#!/usr/bin/python3
+
+"""Defines a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
