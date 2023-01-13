4874208af88b423ca58dd797193323dc885537db
StarPackEdu <111011755+StarPackEdu@users.noreply.github.com>
2023-01-13 10:06:27 +0100

    Update 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,2 +1,8 @@
-thr'
-th
+#!/usr/bin/python3
+
+"""Defines a Python class-to-JSON function."""
+
+
+def class_to_json(obj):
+    """Return the dictionary representation of a simple data structure."""
+    return obj.__dict__
